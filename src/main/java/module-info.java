6fce4b7567dc7c882162171d1e31fada7c9ee57b
hedvigannast <hedvig.annast@gmail.com>
2025-05-22 23:34:23 +0300

    esimene

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.oopprojekt {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.oopprojekt to javafx.fxml;
+    exports com.example.oopprojekt;
+}
